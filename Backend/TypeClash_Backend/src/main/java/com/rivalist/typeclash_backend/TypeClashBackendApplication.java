856a05ce69b0faf0c0cf9ea428f45194fd2596c0
Aaditya01Sehgal <aadityasehgal567@gmail.com>
2025-04-19 04:03:20 +0530

    Commit the folder Structure of Backend

---
+++ Backend/TypeClash_Backend/src/main/java/com/rivalist/typeclash_backend/TypeClashBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rivalist.typeclash_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeClashBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TypeClashBackendApplication.class, args);
+    }
+
+}
